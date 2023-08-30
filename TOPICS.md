5f3ee8f5881474da76cc985b3c51a48d328b0d12
Sebastian <hbhbnr@posteo.net>
2023-08-30 19:29:28 +0200

    Start with day 01a

---
+++ TOPICS.md
@@ -0,0 +1,8 @@
+# Topics
+
+Some notes on the techniques, concepts, classes, etc. encountered while solving [Advent of Code 2022](https://adventofcode.com/2022).
+
+## Days
+
+### Day 01
+* Fortran: the basics, like statements, variables, I/O, functions and subroutines, loops, etc.
--- src/day01a.f90
+++ src/day01a.f90
@@ -1,3 +1,16 @@
-program helloworld
-write(*,'(Aa)') 'Hello, world!'
-end program helloworld
+subroutine printresultline(day, result)
+    implicit none
+
+    character(len=*) :: day
+    character(len=*) :: result
+
+    print '(A, A, A, A)', 'Day ', day, ': ', result
+end subroutine
+
+program day01a
+    implicit none
+
+    character(2) :: result
+    result = '42'
+    call printresultline('01a', result)
+end program day01a
