ff6b2672d21f3aa656e0d85081cb9011ae133d18
Sebastian <hbhbnr@posteo.net>
2023-09-13 23:04:56 +0200

    Improve day 04a

--- src/day04a.f90
+++ src/day04a.f90
@@ -1,5 +1,6 @@
 !> Solution for https://adventofcode.com/2021/day/4 part a
 module day04a
+    use util, only : printioerror
     implicit none
     private
 
@@ -8,45 +9,28 @@ module day04a
 
 contains
 
-    integer function fully_contains(section1a, section1b, section2a, section2b)
+    integer function fully_contains(range1a, range1b, range2a, range2b)
         implicit none
 
-        integer, intent(in) :: section1a, section1b, section2a, section2b
+        integer, intent(in) :: range1a, range1b, range2a, range2b
 
-        if (((section1a <= section2a) .and. (section1b >= section2b)) &
+        if (((range1a <= range2a) .and. (range1b >= range2b)) &
             .or. &
-            ((section1a >= section2a) .and. (section1b <= section2b))) then
+            ((range1a >= range2a) .and. (range1b <= range2b))) then
             fully_contains = 1
         else
             fully_contains = 0
         end if
     end function
 
-    subroutine printioerror(iostat, iomsg, alwaysstop)
-        implicit none
-
-        integer, intent(in)           :: iostat
-        character(len=*), intent(in)  :: iomsg
-        logical, intent(in), optional :: alwaysstop
-        character(len=80)             :: iostatstr
-
-        if (iostat > 0) then
-            write (iostatstr, *) iostat
-            print '(A, A, A, A, A)', 'I/O error: ', trim(adjustl(iostatstr)), ' (', trim(iomsg), ')'
-        end if
-        if ((iostat > 0) .or. (present(alwaysstop) .and. alwaysstop)) then
-            stop
-        end if
-    end subroutine
-
     integer function solve(filename)
         implicit none
 
         character(len=*), intent(in) :: filename
-        character(len=5)             :: sections1, sections2
-        integer                      :: dashpos, section1a, section1b, section2a, section2b
+        character(len=11)            :: line
         integer                      :: io, iostat
         character(len=512)           :: iomsg
+        integer                      :: i, dashpos, range1a, range1b, range2a, range2b
         integer                      :: line_fully_contains, sum_fully_contains
 
         sum_fully_contains = 0
@@ -55,21 +39,20 @@ contains
             call printioerror(iostat, iomsg, .true.)
         end if
         do
-            read(io, *, iostat=iostat, iomsg=iomsg) sections1, sections2
+            read(io, '(A)', iostat=iostat, iomsg=iomsg) line
             if (iostat /= 0) then
                 ! end of file or I/O error -> exit loop
                 call printioerror(iostat, iomsg)
                 exit
             end if
             ! debug: output line after parsing
-            ! print *, sections1, ',', sections2
-            dashpos = index(sections1, '-')
-            sections1(dashpos:dashpos) = ','
-            dashpos = index(sections2, '-')
-            sections2(dashpos:dashpos) = ','
-            read(sections1, *) section1a, section1b
-            read(sections2, *) section2a, section2b
-            line_fully_contains = fully_contains(section1a, section1b, section2a, section2b)
+            ! print *, line
+            do i = 1, 2
+                dashpos = index(line, '-')
+                line(dashpos:dashpos) = ','
+            end do
+            read(line, *) range1a, range1b, range2a, range2b
+            line_fully_contains = fully_contains(range1a, range1b, range2a, range2b)
             sum_fully_contains = sum_fully_contains + line_fully_contains
         end do
         close(io)
