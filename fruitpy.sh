7d5c2e389388b7474176e914c653de9c521f28d1
Sebastian <hbhbnr@posteo.net>
2023-09-13 22:43:24 +0200

    Improve fruitpy.sh

--- fruitpy.sh
+++ fruitpy.sh
@@ -4,4 +4,4 @@ FRUITPYTESTS=$(make -pn | grep 'FRUITPYTESTS :=' | cut -d' ' -f 3-)
 
 for FRUITPYTEST in $FRUITPYTESTS; do
     python3 ${FRUITPYTEST}
-done
+done | grep -vE "^make: '[^']+' is up to date.$"
