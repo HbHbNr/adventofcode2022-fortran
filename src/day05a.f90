2159ffadb604f29e23547a61f3d3530071ceda93
Sebastian <hbhbnr@posteo.net>
2023-09-19 00:39:28 +0200

    Continue with day 05a

--- src/day05a.f90
+++ src/day05a.f90
@@ -8,40 +8,80 @@ module day05a
 
 contains
 
+    subroutine analyse_init_block(io, lines, stackcount)
+        implicit none
+
+        integer              :: io, iostat
+        character(len=512)   :: iomsg
+        integer, intent(out) :: lines, stackcount
+        character(len=64)    :: line, lastline
+        character(len=1)     :: lastchar
+
+        lines = 0
+        stackcount = 0
+        line = ''
+        lastline = ''
+        do
+            lines = lines + 1
+            lastline = line
+            read(io, '(A)', iostat=iostat, iomsg=iomsg) line
+            if (iostat /= 0) then
+                ! end of file or I/O error -> stop program
+                call printioerror(iostat, iomsg, .true.)
+                exit
+            end if
+            if (len_trim(line) == 0) then
+                ! empty line: init block over
+                lines = lines - 2
+                ! print *, lastline
+                lastchar = lastline(len_trim(lastline):len_trim(lastline))
+                ! print *, 'lastchar=', lastchar
+                read(lastchar, '(I1)') stackcount
+                exit
+            end if
+            ! print *, lines
+        end do
+    end subroutine
+
     character(len=9) function solve(filename)
         implicit none
 
         character(len=*), intent(in) :: filename
-        ! integer                      :: io, iostat
-        ! character(len=512)           :: iomsg
+        integer                      :: io, iostat
+        character(len=512)           :: iomsg
+        integer                      :: lines, stackcount
         ! character(len=5)             :: line
         ! integer                      :: linecalories, sumcalories = 0, maxcalories = 0
 
-        ! open(newunit=io, file=filename, status='old', action='read', iostat=iostat, iomsg=iomsg)
-        ! if (iostat /= 0) then
-        !     call printioerror(iostat, iomsg, .true.)
-        ! end if
-        ! do
-        !     read(io, '(A)', iostat=iostat, iomsg=iomsg) line
-        !     if (iostat /= 0) then
-        !         ! end of file or I/O error -> exit loop
-        !         call printioerror(iostat, iomsg)
-        !         exit
-        !     end if
-        !     ! debug: output line from file and its length
-        !     ! print '(A5, A2, I1)', line, ': ', len_trim(line)
-        !     if (len_trim(line) == 0) then
-        !         ! reset sum for a new Elf
-        !         sumcalories = 0
-        !     else
-        !         ! same Elf, so add calories to the sum
-        !         read(line, *) linecalories
-        !         sumcalories = sumcalories + linecalories
-        !         maxcalories = max(maxcalories, sumcalories)
-        !     end if
-        ! end do
-        ! close(io)
-        ! ! return maximum calories
+        open(newunit=io, file=filename, status='old', action='read', iostat=iostat, iomsg=iomsg)
+        if (iostat /= 0) then
+            call printioerror(iostat, iomsg, .true.)
+        end if
+        do
+            call analyse_init_block(io, lines, stackcount)
+            print *, lines, stackcount
+            rewind(io)
+            exit
+            ! read(io, '(A)', iostat=iostat, iomsg=iomsg) line
+            ! if (iostat /= 0) then
+            !     ! end of file or I/O error -> exit loop
+            !     call printioerror(iostat, iomsg)
+            !     exit
+            ! end if
+            ! ! debug: output line from file and its length
+            ! ! print '(A5, A2, I1)', line, ': ', len_trim(line)
+            ! if (len_trim(line) == 0) then
+            !     ! reset sum for a new Elf
+            !     sumcalories = 0
+            ! else
+            !     ! same Elf, so add calories to the sum
+            !     read(line, *) linecalories
+            !     sumcalories = sumcalories + linecalories
+            !     maxcalories = max(maxcalories, sumcalories)
+            ! end if
+        end do
+        close(io)
+        ! return maximum calories
         solve = '_________'
     end function
 
--- src/day05a_main.f90
+++ src/day05a_main.f90
@@ -5,7 +5,7 @@ program day05a_main
 
     character(len=9) :: result
 
-    result = solve('inputfiles/day05_example.txt')
-    ! result = solve('inputfiles/day05_input.txt')
+    ! result = solve('inputfiles/day05_example.txt')
+    result = solve('inputfiles/day05_input.txt')
     call printresultline('05a', result)
 end program day05a_main
