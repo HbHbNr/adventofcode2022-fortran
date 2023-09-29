7d0a8ab7d139b7d2e2b72dacf662d26664fe77d7
Sebastian <hbhbnr@posteo.net>
2023-09-30 01:45:53 +0200

    Work on day 08a

--- src/day08a.f90
+++ src/day08a.f90
@@ -1,6 +1,6 @@
 !> Solution for https://adventofcode.com/2021/day/8 part a
 module day08a
-    use iso_fortran_env, only : int8
+    use iso_fortran_env, only : int8, logical_kinds
     use util, only : printioerror, readinputfile_asintarray
     implicit none
     private
@@ -9,50 +9,65 @@ module day08a
 
 contains
 
+    subroutine checkrow(heightarray, visiblearray, rownumber, reversed)
+        implicit none
+
+        integer(int8), intent(in)      :: heightarray(:,:)
+        logical(kind=1), intent(inout) :: visiblearray(:,:)
+        integer, intent(in)            :: rownumber
+        logical, intent(in)            :: reversed
+        integer                        :: height, currenthighest
+        integer                        :: row, column, start, end, step
+
+        row = rownumber
+        currenthighest = 0
+        if (.not. reversed) then
+            start = 1
+            end = size(heightarray, 2)
+            step = 1
+        else
+            start = size(heightarray, 2)
+            end = 1
+            step = -1
+        end if
+        do column = start, end, step
+            height = heightarray(row, column)
+            if (height > currenthighest) then
+                currenthighest = height
+                visiblearray(row, column) = .true.
+            end if
+        end do
+    end subroutine
+
     integer function solve(filename, linebufferlength)
         implicit none
 
         character(len=*), intent(in) :: filename
         integer, intent(in)          :: linebufferlength
-        integer(int8), allocatable   :: intarray(:,:)
-        integer                      :: i
+        integer(int8), allocatable   :: heightarray(:,:)
+        logical(kind=1), allocatable :: visiblearray(:,:)
+        integer                      :: rownumber
 
-        intarray = readinputfile_asintarray(filename, linebufferlength)
-        do i = 1, size(intarray, 1)
+        ! print *, logical_kinds
+        heightarray = readinputfile_asintarray(filename, linebufferlength)
+        do rownumber = 1, size(heightarray, 1)
             ! print all rows
-            print *, intarray(i, :)
+            print *, heightarray(rownumber, :)
         end do
-        print *, intarray(4, 4)
-        ! integer                      :: io, iostat
-        ! character(len=512)           :: iomsg
-        ! character(len=5)             :: line
-        ! integer                      :: linecalories, sumcalories = 0, maxcalories = 0
-
-        ! open(newunit=io, file=filename, status='old', action='read', iostat=iostat, iomsg=iomsg)
-        ! if (iostat /= 0) then
-        !     call printioerror(iostat, iomsg, .true.)
-        ! end if
-        ! do
-        !     read(io, '(A)', iostat=iostat, iomsg=iomsg) line
-        !     if (iostat /= 0) then
-        !         ! end of file or I/O error -> exit loop
-        !         call printioerror(iostat, iomsg)
-        !         exit
-        !     end if
-        !     ! debug: output line from file and its length
-        !     ! print '(A, A2, I1)', trim(line), ': ', len_trim(line)
-        !     if (len_trim(line) == 0) then
-        !         ! reset sum for a new Elf
-        !         sumcalories = 0
-        !     else
-        !         ! same Elf, so add calories to the sum
-        !         read(line, *) linecalories
-        !         sumcalories = sumcalories + linecalories
-        !         maxcalories = max(maxcalories, sumcalories)
-        !     end if
-        ! end do
-        ! close(io)
-        ! ! return maximum calories
+        allocate(visiblearray(size(heightarray,1), size(heightarray,2)), source=logical(.false., kind=1))
+        do rownumber = 1, size(visiblearray, 1)
+            ! print all rows
+            print *, visiblearray(rownumber, :)
+        end do
+        print *
+        do rownumber = 1, size(visiblearray, 1)
+            call checkrow(heightarray, visiblearray, rownumber, .false.)
+            call checkrow(heightarray, visiblearray, rownumber, .true.)
+        end do
+        do rownumber = 1, size(visiblearray, 1)
+            ! print all rows
+            print *, visiblearray(rownumber, :)
+        end do
         solve = -1
     end function
 
