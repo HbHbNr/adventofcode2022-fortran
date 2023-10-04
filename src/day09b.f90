ae8c66fcdf007c60110e154d4ce5bad080e93503
Sebastian <hbhbnr@posteo.net>
2023-10-05 01:00:56 +0200

    Work on day 09b

--- src/day09b.f90
+++ src/day09b.f90
@@ -62,13 +62,13 @@ contains
 
         type(ComplexList), intent(inout) :: motions
         type(ComplexList), intent(inout) :: visitlist
-        integer                          :: i, l, lastlink
+        integer                          :: i, link, lastlink
         complex                          :: motion, chainlinks(10)
 
         ! position all chainlinks at centre of coordinate system
         lastlink = size(chainlinks)
-        do l = 1, lastlink
-            chainlinks(l) = (0,0)
+        do link = 1, lastlink
+            chainlinks(link) = (0,0)
         end do
 
         ! execute all motions from the series
@@ -79,14 +79,17 @@ contains
             chainlinks(1) = chainlinks(1) + motion
 
             ! move all other chainlinks based on the drag of their forerunners
-            do l = 2, size(chainlinks)
-                chainlinks(l) = move_tail(chainlinks(l - 1), chainlinks(l))
+            do link = 2, lastlink
+                chainlinks(link) = move_tail(chainlinks(link - 1), chainlinks(link))
             end do
 
             ! store new tail position only in visitlist if it is the very first visit
             if (visitlist%containsints(chainlinks(lastlink)) .eqv. .false.) then
                 call visitlist%add(chainlinks(lastlink))
             end if
+
+            ! call plot_playground(visitlist, chainlinks)
+            ! print *
         end do
     end subroutine execute_motions
 
@@ -118,26 +121,35 @@ contains
         end if
     end function
 
-    subroutine plot_visitlist(visitlist)
+    subroutine plot_playground(visitlist, chainlinks)
         implicit none
 
-        type(ComplexList), intent(in) :: visitlist
-        integer                       :: minx, miny, maxx, maxy, v, row, column
-        character(len=1), allocatable :: fields(:,:)
-        character(len=1), parameter   :: emptyfield = '.', visitedfield = '#', startfield = 's'
-        complex                       :: field
+        type(ComplexList), intent(in)           :: visitlist
+        complex, intent(in), optional           :: chainlinks(:)
+        integer                                 :: minx, miny, maxx, maxy, v, row, column
+        character(len=1), allocatable           :: fields(:,:)
+        character(len=1), parameter             :: emptyfield = '.', visitedfield = '#', startfield = 's'
+        complex                                 :: field
 
         ! find dimensions
-        minx = 0
-        miny = 0
-        maxx = 0
-        maxy = 0
+        minx = -10
+        miny = -10
+        maxx = 10
+        maxy = 10
         do v = 1, visitlist%length()
             minx = min(minx, int(real(visitlist%get(v))))
             miny = min(miny, int(aimag(visitlist%get(v))))
             maxx = max(maxx, int(real(visitlist%get(v))))
             maxy = max(maxy, int(aimag(visitlist%get(v))))
         end do
+        if (present(chainlinks)) then
+            do v = 1, size(chainlinks)
+                minx = min(minx, int(real(chainlinks(v))))
+                miny = min(miny, int(aimag(chainlinks(v))))
+                maxx = max(maxx, int(real(chainlinks(v))))
+                maxy = max(maxy, int(aimag(chainlinks(v))))
+            end do
+        end if
         ! print *, minx, miny, maxx, maxy
 
         ! prepare playground
@@ -148,13 +160,31 @@ contains
                 fields(row, column) = emptyfield
             end do
         end do
+
+        ! mark all visited fields
         do v = 1, visitlist%length()
             field = visitlist%get(v)
             row = int(aimag(field))
             column = int(real(field))
             fields(row, column) = visitedfield
         end do
+
+        ! makr startfield
         fields(0, 0) = startfield
+
+        ! mark all occupied fields
+        if (present(chainlinks)) then
+            do v = size(chainlinks), 1, -1
+                field = chainlinks(v)
+                row = int(aimag(field))
+                column = int(real(field))
+                if (v == 1) then
+                    fields(row, column) = 'H'
+                else
+                    fields(row, column) = char(ichar('1') + v - 2)
+                end if
+            end do
+        end if
 
         ! print playground
         do row = miny, maxy
@@ -168,7 +198,7 @@ contains
         character(len=*), intent(in)   :: filename
         type(ComplexList) :: motions
         type(ComplexList) :: visitlist
-        integer           :: i
+        ! integer           :: i
 
         ! analyse series of motions and prepare containers
         call analyse_motions(filename, motions, visitlist)
@@ -178,10 +208,10 @@ contains
         ! do i = 1, visitlist%length()
         !     print *, visitlist%get(i)
         ! end do
-        call plot_visitlist(visitlist)
+        call plot_playground(visitlist)
 
         ! return number of visited positions
-        solve = visitlist%length()
+        solve = -1  ! visitlist%length()
         ! 2617 is wrong
     end function
 
