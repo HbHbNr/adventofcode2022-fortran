e305de4376645d979d24eb56db62bf01062f895c
Sebastian <hbhbnr@posteo.net>
2023-11-06 00:34:06 +0100

    Work on day 13b

---
+++ src/day13b_main.f90
@@ -0,0 +1,11 @@
+program day13b_main
+    use util, only : printresultline_integer
+    use day13b, only : solve
+    implicit none
+
+    integer :: result
+
+    result = solve('inputfiles/day13_example.txt')
+    ! result = solve('inputfiles/day13_input.txt')
+    call printresultline_integer('13b', result)
+end program day13b_main
