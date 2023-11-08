eaa3effb5c7ee0f6b0e7db232ffd025c5213b9d6
Sebastian <hbhbnr@posteo.net>
2023-11-09 00:39:01 +0100

    Work on day 14a

--- src/day14a.f90
+++ src/day14a.f90
@@ -5,7 +5,7 @@ module day14a
     private
 
     integer, parameter :: maxlinelength = 150
-    integer, parameter :: avgcoordsperline = 9
+    integer, parameter :: avgcoordsperline = 10
 
     public :: solve
 
@@ -19,16 +19,17 @@ contains
         integer                           :: i, j, c, count
 
         ! average of "avgcoordsperline" coords per line
-        allocate(coords(size(lines) * avgcoordsperline * 2), source=-1)
+        allocate(coords(size(lines) * avgcoordsperline * 2), source=0)
 
         ! scan lines an save coords
-        c = 0
+        c = 0  ! number of single coords saved into linear array
         do i = 1, size(lines)
             line = lines(i)
             print *, line
             count = 0
             do j = 1, len_trim(line)
                 char = line(j:j)
+                ! count coord pairs and override non-digits
                 select case (char)
                 case (',')
                     count = count + 2
@@ -40,13 +41,31 @@ contains
             print *, 'count=', count
             print *, line
             read (line, *) coords(c + 1:c + count)
+            c = c + count
 
-            ! keep one value -1 between coords
-            c = c + count + 1
+            ! duplicate the two values of the last coordinate to signal the end of the line segments
+            coords(c+1:c+2) = coords(c-1:c)
+            c = c + 2
         end do
-        print *, coords
 
+        ! shrink coords array to the actual number of values
+        coords = coords(1:c)
     end subroutine
+
+    function simulate_sand(coords) result(sandlimit)
+        implicit none
+
+        integer, intent(in) :: coords(:)
+        integer             :: sandlimit
+        integer             :: minx, maxx, miny, maxy
+
+        minx = minval(coords(::2))
+        maxx = maxval(coords(::2))
+        miny = minval(coords(2::2))
+        maxy = maxval(coords(2::2))
+        print *, minx, maxx, miny, maxy
+        sandlimit = -1
+    end function
 
     integer function solve(filename)
         implicit none
@@ -54,10 +73,14 @@ contains
         character(len=*), intent(in)  :: filename
         character(len=:), allocatable :: lines(:)
         integer, allocatable          :: coords(:)
+        integer                       :: sandlimit
 
         lines = readinputfile_asstringarray(filename, maxlinelength)
 
         call extract_coords(lines, coords)
+        print *, coords
+
+        sandlimit = simulate_sand(coords)
 
         solve = -1
     end function
